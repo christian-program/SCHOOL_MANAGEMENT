307fb3f58def050fc6ed41a26a3cbecd9f3cd62c
Christian <pamachristian.97@gmail.com>
2026-01-15 10:07:47 +0100

    fixing video bug

--- portal/static/sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
-});
-
-self.addEventListener('fetch', (e) => {
-    // Ici, on pourrait ajouter une gestion du cache pour le mode hors-ligne
-    e.respondWith(fetch(e.request));
-});
